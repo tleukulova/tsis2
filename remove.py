262a3a14c10ca4ade040e743bd3b32080f2fb1c8
tleukulova <155585792+tleukulova@users.noreply.github.com>
2024-01-29 00:57:38 +0600

    remove.py

--- remove.py
+++ remove.py
@@ -1,3 +1,8 @@
 thislist = ["apple", "banana", "cherry"]
 thislist.remove("banana")
 print(thislist)
+
+#If there are more than one item with the specified value, the remove() method removes the first occurance:
+thislist = ["apple", "banana", "cherry", "kiwi", "banana"]
+thislist.remove("banana")
+print(thislist)
